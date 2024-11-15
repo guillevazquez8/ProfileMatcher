1857ba6ebfb5f71a03f0e04d0929afebf1c1560d
gvazquez <guillevazquez8@gmail.com>
2024-11-15 15:38:40 +0100

    started modelling campaign table

---
+++ app/__init__.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import config
+
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+
+def create_app(config_class=config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    with app.app_context():
+        # import routes
+
+        # register blueprints
+        #app.register_blueprint()
+
+        return app
---
+++ config.py
@@ -0,0 +1,1 @@
+SQLALCHEMY_DATABASE_URI = 'postgresql://guille:celta@localhost:5432/profile_matcher'
